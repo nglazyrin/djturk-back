42e52a0efc577177f9800252426930b03ab7237d
Veleslavia <okalistratova@gmail.com>
2013-09-21 15:16:49 +0400

    update zvooq's interaction files

--- src/python/zvooq_getmusic.py
+++ src/python/zvooq_getmusic.py
@@ -8,18 +8,28 @@ values = dumps({
     "password": "gkfdcr"
 })
 
-headers = {"Content-Type": "application/json", "X-Method": "call"}
-r = requests.post("http://kazan.zvq.me/auth/login", data=values, headers=headers)
-print r.url
-print r.text
+def get_id(text):
+    
+    
+    
+def get_track_id(title, artist):
+    headers = {"Content-Type": "application/json", "X-Method": "call"}
+    r = requests.post("http://kazan.zvq.me/auth/login", data=values, headers=headers)
+    headers = {"Content-Type": "application/json"}
 
-headers = {"Content-Type": "application/json"}
-r = requests.get('http://kazan.zvq.me/api/search?body={"string":"lady gaga bad romance"}', headers=headers)
-print r.text
- 
+    r = requests.get('http://kazan.zvq.me/api/search?body={"string":"'+title+'"}"', headers=headers)
+    title_id = get_id(r.text)
+    r = requests.get('http://kazan.zvq.me/api/search?body={"string":"'+artist+'"}"', headers=headers)
+    artist_id = get_id(r.text)
+    if len(title_id) != 0:
+        for id in title_id:
+            if len(artist_id) != 0:
+                if id in artist_id:
+                    return id
+            return title_id[0]
+    return "Song not found"
+    
 headers = {"X-Auth": ":s:a:a:k7y0EHv6tkfDWsP0XhmasUdVvpk=::1413eca6ea7::7874555"}
-#request = Request('http://kazan.zvq.me/api/data/track/url?body={"id":5805325,"type":"stream"}', headers=headers)
-#response_body = urlopen(request).read()
-#print response_body
-r = requests.get('http://kazan.zvq.me/api/data/track/url?body={"id":1416499,"type":"stream"}', headers=headers)
+r = requests.get('http://kazan.zvq.me/api/data/track/url?body={"id":'+str(id)+',"type":"stream"}', headers=headers)
+
 print r.text
